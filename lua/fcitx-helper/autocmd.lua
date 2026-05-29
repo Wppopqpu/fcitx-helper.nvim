7119cba05c97deaea4d3609e10a3b3ccc1d8c9b4
Wppopqpu <wppopqpu@outlook.com>
2026-05-29 13:54:01 +0800

    feat: add autocmd

---
+++ lua/fcitx-helper/autocmd.lua
@@ -0,0 +1,25 @@
+local M = {}
+local state = require("fcitx-helper.state")
+---@type fcitx-helper.Opt
+local config = require("fcitx-helper.config")
+
+M.augroup = vim.api.nvim_create_augroup("fcitx-helper", { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*",
+	group = M.augroup,
+	callback = function (ev)
+		local mode = vim.fn.mode():sub(1, 1)
+		if mode == "n" then
+			if config.inactivate_in_normal then
+				state.apply_state("inactive")
+			end
+			return
+		end
+		if mode == "i" or mode == "t" then
+			state.apply_current_state()
+			return
+		end
+	end,
+})
+
+return M
--- lua/fcitx-helper/init.lua
+++ lua/fcitx-helper/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 M.setup = require("fcitx-helper.config").setup
 M.state = require("fcitx-helper.state")
+M.autocmd = require("fcitx-helper.autocmd")
 
+M.state.apply_state("inactive")
 return M
